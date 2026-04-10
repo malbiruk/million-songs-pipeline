959401399527a4be0652adc206adf9fbf7884764
malbiruk <2601074@gmail.com>
2026-04-10 23:29:38 +0400

    Update disk quota

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,11 +42,17 @@ resource "google_dataproc_cluster" "spark_cluster" {
     master_config {
       num_instances = 1
       machine_type  = "n1-standard-2"
+      disk_config {
+        boot_disk_size_gb = 100
+      }
     }
 
     worker_config {
       num_instances = 2
       machine_type  = "n1-standard-2"
+      disk_config {
+        boot_disk_size_gb = 100
+      }
     }
 
     software_config {
